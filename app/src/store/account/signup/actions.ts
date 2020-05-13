9ec1ddc38a179d8111bb9ea209fe540ad426adb7
Steve Huckle <steve.huckle@gmail.com>
2020-05-13 15:50:41 +0100

    Getting closer

--- app/src/store/account/signup/actions.ts
+++ app/src/store/account/signup/actions.ts
@@ -7,43 +7,48 @@ import { SignupProps } from '../types'
 import { FormActionTypes, TxData } from '../../helpers/forms/types'
 
 import { Account } from '../../../config/strings'
-import { Remote } from '../../../config/paths'
+import { Remote, Paths } from '../../../config/paths'
+import { history } from '../../../utils/history'
 
 export const signup = (details: SignupProps) => {
   return async (dispatch: AppDispatch) => {
 
-    let actionType = FormActionTypes.FORM_FAILURE
-    let txData: TxData = {
-        summary: "",
-        info: {}
-    }
-    try {
+        let actionType = FormActionTypes.FORM_FAILURE
+        let txData: TxData = {
+            summary: "",
+            info: {}
+        }
+
+        console.log(details)
+
       // set(bytes32 _reference, bytes32 _orgRef, bytes32 _reportingOrgRef, bytes32 _version, bytes32 _generatedTime)
       //const apiCall = await ...
-      const response = await fetch(`${Remote.secure}${Remote.server}:${Remote.port}${Remote.signup}`, {
+      /*fetch(`${Remote.secure}${Remote.server}:${Remote.port}${Remote.signup}`, {
         method: 'POST',
         headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
+          'Content-Type': 'application/json'
         },
-        body: JSON.stringify({
-            email: details.email,
-  	        password: details.password,
-        })
+        body: JSON.stringify(details)
       })
-      txData = {
-        summary: `${Account.signupSuccess}`,
-        info: response.json()
-      }
-      actionType = FormActionTypes.FORM_SUCCESS
-    } catch (error) {
-      txData = {
-          summary: `${Account.signupFail}, error`,
-          info:  {}
-      }
-      console.log(`${Account.signupFail}`, error)
-    }
+      .then(response => response.json())
+      .then(data => {
+          console.log('Success:', data);
+          txData = {
+            summary: `${Account.signupSuccess}`,
+            info: data
+          }
+          actionType = FormActionTypes.FORM_SUCCESS
+          history.push(`${Paths.home}`)
+      })
+     .catch(error => {
+          txData = {
+              summary: `${Account.signupFail}, error`,
+              info:  {}
+          }
+          history.push(`${Paths.signup}`)
+          console.log(`${Account.signupFail}`, error)
+     })*/
 
-    dispatch(write({data: {data: txData}})(actionType))
+     dispatch(write({data: {data: txData}})(actionType))
   }
 }
